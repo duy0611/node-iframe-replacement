2fa4abc9b9254f90c3a1fb21ce7671abf2c3bb66
Lifesup Team <lifesupteam@gmail.com>
2017-06-01 00:20:57 +0300

    add structure to load separated config file for each shop

---
+++ config/js/drag-drop.js
@@ -0,0 +1,115 @@
+function tagonAllowDrop(ev) {
+	ev.preventDefault();
+	ev.dataTransfer.dropEffect = 'copy';  // See the section on the DataTransfer object.
+	return false;
+}
+
+function tagonDragStart(ev, targetServer) {
+	var dataObj = {
+		srcImg: '',
+		productLink: ''
+	};
+	
+	var pattern = window.location.href.split('url=')[1].split('//')[1].split('/')[0];
+	
+	if(ev.target.nodeName === 'IMG') {
+		dataObj.srcImg = ev.target.src;
+		if(ev.target.parentNode != null && ev.target.parentNode.nodeName === 'A' && ev.target.parentNode.href.includes(pattern) && !ev.target.parentNode.href.includes(ev.target.src)) {
+			dataObj.productLink = ev.target.parentNode.href;
+		}
+		else {
+			var el = ev.target;
+			while(el.nodeName !== 'BODY'){
+				if(el.parentNode.nodeName === 'A' && el.parentNode.href.includes(pattern) && !el.parentNode.href.includes(ev.target.src)){
+				
+					dataObj.productLink = el.parentNode.href;
+					break;
+				}
+				else{
+					el = el.parentNode;
+				}
+			}
+			if(!dataObj.productLink){
+				dataObj.productLink = window.location.href;
+			}
+		}
+		ev.dataTransfer.setData("text", JSON.stringify(dataObj));
+		ev.dataTransfer.setData("targetServer", targetServer);
+		
+		document.getElementById("tagon-user-collection-bar").style.display = "initial";
+	}
+	else if (ev.target.nodeName === 'A') {
+		if(ev.target.href.includes(pattern)){
+			dataObj.productLink = ev.target.href;
+		}
+		else{
+			dataObj.productLink = window.location.href;
+		}
+		if(ev.target.getElementsByTagName('img')[0] !== undefined) {
+			dataObj.srcImg = $(ev.target).find('img').first()[0].src;
+			ev.dataTransfer.setData("text", JSON.stringify(dataObj));
+			ev.dataTransfer.setData("targetServer", targetServer);
+			document.getElementById("tagon-user-collection-bar").style.display = "initial";
+		}
+		else if(ev.target.getAttribute('data-src')){
+			dataObj.srcImg = 'http://' + pattern + '/' + ev.target.getAttribute('data-src');
+			ev.dataTransfer.setData("text", JSON.stringify(dataObj));
+			ev.dataTransfer.setData("targetServer", targetServer);
+			document.getElementById("tagon-user-collection-bar").style.display = "initial";
+		}
+		else if(ev.target.getAttribute('src')){
+			dataObj.srcImg = ev.target.getAttribute('src');
+			ev.dataTransfer.setData("text", JSON.stringify(dataObj));
+			ev.dataTransfer.setData("targetServer", targetServer);
+			document.getElementById("tagon-user-collection-bar").style.display = "initial";
+		}
+		else {
+			alert("CAN ONLY DRAG PRODUCT IMAGE from A!!!");
+		}
+	}
+	else {
+		if(ev.target.getElementsByTagName('img')[0] != undefined) {
+			dataObj.srcImg = $(ev.target).find('img').first()[0].src;
+			ev.dataTransfer.setData("text", JSON.stringify(dataObj));
+			ev.dataTransfer.setData("targetServer", targetServer);
+			document.getElementById("tagon-user-collection-bar").style.display = "initial";
+		}
+		else {
+			alert("CAN ONLY DRAG PRODUCT IMAGE!!!");
+		}
+	}
+}
+
+function tagonDragEnd(ev) {
+	document.getElementById("tagon-user-collection-bar").style.display = "none";
+}
+
+function tagonDrop(ev) {
+	ev.preventDefault();
+	var data = ev.dataTransfer.getData('text');
+	var targetServer = ev.dataTransfer.getData('targetServer');
+	
+	if(data) {
+		var receiverWin = window.parent;
+		function sendMessage(e) {
+			// Prevent any default browser behaviour.
+			// e.preventDefault();
+			// Send a message with the text of the source to the new window.
+			// console.log(data);
+			receiverWin.postMessage(data, targetServer);
+		}
+		sendMessage();
+	}
+	
+	document.getElementById("tagon-user-collection-bar").style.display = "none";
+}
+
+function tagonFadeInPreload(targetServer) {
+	var receiverWin = window.parent;
+	receiverWin.postMessage("fadeIn", targetServer);
+}
+
+function tagonFadeOutPreload(serverName, serverPort, targetServer, enableHttps) {
+	var receiverWin = window.parent;
+	receiverWin.postMessage("fadeOut", targetServer);
+}
--- example/server.js
+++ example/server.js
@@ -45,6 +45,57 @@ function Server() {
         });
     });
 	
+	// create simple route to test our fake news
+    app.get('/load', function(req, res) {
+		
+		var filePath = '../config/' + req.query.file;
+		
+		var extname = path.extname(filePath);
+		var contentType = 'text/html';
+		switch (extname) {
+			case '.js':
+				contentType = 'text/javascript';
+				break;
+			case '.css':
+				contentType = 'text/css';
+				break;
+			case '.json':
+				contentType = 'application/json';
+				break;
+			case '.png':
+				contentType = 'image/png';
+				break;      
+			case '.jpg':
+				contentType = 'image/jpg';
+				break;
+			case '.wav':
+				contentType = 'audio/wav';
+				break;
+		}
+
+		fs.readFile(path.join(__dirname, filePath), function(error, content) {
+			
+			if (error) {
+				if(error.code == 'ENOENT'){
+					fs.readFile('./404.html', function(error, content) {
+						res.writeHead(200, { 'Content-Type': contentType });
+						res.end(content, 'utf-8');
+					});
+				}
+				else {
+					res.writeHead(500);
+					res.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+					res.end(); 
+				}
+			}
+			else {
+				res.writeHead(200, { 'Content-Type': contentType });
+				res.end(content, 'utf-8');
+			}
+		});
+        
+    });
+	
 	// start the server
 	if(enableHttps) {
 		var options = {
