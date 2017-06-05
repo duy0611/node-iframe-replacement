89b99295270130a25fdfcf167c86b07f0220fda0
Nguyen Thanh Binh <johan.nguyen.311@gmail.com>
2017-06-05 23:48:04 +0300

    Update 20again.js

--- config/20again.js
+++ config/20again.js
@@ -2,7 +2,7 @@ function fix20Again(){
 	var els = tagonJquery("ul.actions > li > h3");
 	for(var i=0;i<els.length;i++){
 		var url = els[i].getAttribute('onclick');
-		var path = url.split("=")[1].split("'").join("");
+		var path = url.split("href=")[1].split("'").join("");
 		var newUrl = "location.href='http://localhost:12345/proxy?url=http://20again.vn"+ path + "'";
 		els[i].setAttribute('onclick', newUrl);
 	}
